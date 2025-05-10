2167233940678d2c76606b317067152304b63803
Guido Gasper <guido.gasper@googlemail.com>
2025-05-10 18:55:23 +0200

    fix

--- generate.sh
+++ generate.sh
@@ -18,11 +18,17 @@ CMD="docker run --rm \
 -u ${USER_ID}:${GROUP_ID} \
 -v ${OUTPUT_DIR}:/local \
 openapitools/openapi-generator-cli generate \
--i ${SWAGGER_URL} -g php-nextgen -o /local \
---additional-properties=invokerPackage=EsiClient,packageName=EsiClient,composerVendorName=yourvendor,composerProjectName=esi-client"
+-i ${SWAGGER_URL} -g php -o /local \
+--additional-properties=invokerPackage=EsiClient,packageName=EsiClient,composerVendorName=khazl,composerProjectName=esi-client"
 
 echo "Running Docker command:"
 echo "$CMD"
 eval "$CMD"
+
+COMPOSER_FILE="${OUTPUT_DIR}/composer.json"
+if [ -f "$COMPOSER_FILE" ]; then
+    echo "Patching composer.json with correct package name..."
+    jq '.name = "khazl/esi-client"' "$COMPOSER_FILE" > "${COMPOSER_FILE}.tmp" && mv "${COMPOSER_FILE}.tmp" "$COMPOSER_FILE"
+fi
 
 echo "EVE ESI client generation completed."
